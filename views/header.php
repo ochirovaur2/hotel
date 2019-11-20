1fe80e0ea79dedc781b32421b949f1945c5f887c
Ochirov Aiur <55055930+ochirovaur2@users.noreply.github.com>
2019-11-20 15:04:53 +0800

    Update header.php

--- views/header.php
+++ views/header.php
@@ -8,18 +8,30 @@
   <meta name="keywords" content="hotel,boston hotel,new england hotel">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" media="screen and (max-width: 768px)" href="../css/mobile.css">	
   <title>Hotel BT</title>
 </head>
 <body>
   <header>
     <nav id="navbar">
       <div class="container">
-        <h1 class="logo"><a href="?page=home">HBT</a></h1>
-        <ul>
-          <li><a class="navItem" href="?page=home" id="home">Home</a></li>
-          <li><a class="navItem" href="?page=about" id="about">About</a></li>
-          <li><a class="navItem" href="?page=contact" id="contact">Contact</a></li>
-        </ul>
+        
+			   <label  id="show-menu-btn">
+			     	<i class="fas fa-ellipsis-h"></i>
+			   </label>
+			   <label  id="hide-menu-btn">
+			       <i class="fas fa-times"></i>
+			   </label>
+			<h1 class="logo">
+				<a href="?page=home">HBT</a>
+			</h1>
+			
+			<ul class="menu">
+				<li><a  href="?page=home" id="home">Home</a></li>
+				<li><a href="?page=about" id="about">About</a></li>
+				<li><a href="?page=contact" id="contact">Contact</a></li>
+			
+			</ul>
       </div>
     </nav>
   </header>
